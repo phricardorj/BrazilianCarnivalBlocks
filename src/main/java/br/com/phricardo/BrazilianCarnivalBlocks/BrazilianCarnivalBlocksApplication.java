45f230d1d704f42631f702ca2ec4739c12a42705
Pedro Ricardo <brpedroricardo@gmail.com>
2023-02-17 06:59:07 -0300

    feat: starting spring project and pom.xml

---
+++ src/main/java/br/com/phricardo/BrazilianCarnivalBlocks/BrazilianCarnivalBlocksApplication.java
@@ -0,0 +1,13 @@
+package br.com.phricardo.BrazilianCarnivalBlocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazilianCarnivalBlocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazilianCarnivalBlocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/phricardo/BrazilianCarnivalBlocks/BrazilianCarnivalBlocksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.phricardo.BrazilianCarnivalBlocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrazilianCarnivalBlocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
